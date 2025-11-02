8ba64ea032266fbd2b5e6023e6f2723ac8d37ccf
Santiago <68255238+wpqoiepwoei120931-239@users.noreply.github.com>
2025-11-02 08:06:31 -0500

    Add validation annotations to AuthorRequestModel

--- LibraryManagementSystem/src/main/java/com/champsoft/librarymanagementsystem/PresentationLayer/AuthorRequestModel.java
+++ LibraryManagementSystem/src/main/java/com/champsoft/librarymanagementsystem/PresentationLayer/AuthorRequestModel.java
@@ -1,4 +1,6 @@
 package com.champsoft.librarymanagementsystem.PresentationLayer;
+
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class AuthorRequestModel {
+    @NotBlank(message = "Name is required")
+    @Size(min = 1, max = 100, message = "Name must be between 1-100 characters")
     private String Name;
+    
+    @NotBlank(message = "Biography is required")
+    @Size(min = 1, max = 1000, message = "Biography must be between 1-1000 characters")
     private String biography;
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
 }
