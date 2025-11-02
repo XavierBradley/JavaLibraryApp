bcfd9d4c119a4f035059cd531a492a7393770bec
Santiago <68255238+wpqoiepwoei120931-239@users.noreply.github.com>
2025-11-02 08:07:11 -0500

    Add validation annotations to BookRequestModel

--- LibraryManagementSystem/src/main/java/com/champsoft/librarymanagementsystem/PresentationLayer/BookRequestModel.java
+++ LibraryManagementSystem/src/main/java/com/champsoft/librarymanagementsystem/PresentationLayer/BookRequestModel.java
@@ -1,14 +1,24 @@
 package com.champsoft.librarymanagementsystem.PresentationLayer;
+
 import com.champsoft.librarymanagementsystem.DataAccessLayer.Author;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class BookRequestModel {
-    private long id;
-   private String Title;
-   private String ISBN;
-   private int publicationYear;
-   private Author author;
+    @NotBlank(message = "Title is required")
+    @Size(min = 1, max = 200, message = "Title must be between 1-200 characters")
+    private String Title;
+    
+    @NotBlank(message = "ISBN is required")
+    private String ISBN;
+    
+    @Min(value = 1000, message = "Publication year must be at least 1000")
+    @Max(value = 2100, message = "Publication year must be at most 2100")
+    private int publicationYear;
+    
+    @NotNull(message = "Author is required")
+    private Author author;
 }
