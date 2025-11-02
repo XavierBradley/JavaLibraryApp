6673a1cab85aa0896b0ae810dc68cb52872ac1bd
Santiago <68255238+wpqoiepwoei120931-239@users.noreply.github.com>
2025-11-02 08:19:03 -0500

    Add one-to-many relationship with Book in Author

--- LibraryManagementSystem/src/main/java/com/champsoft/librarymanagementsystem/DataAccessLayer/Author.java
+++ LibraryManagementSystem/src/main/java/com/champsoft/librarymanagementsystem/DataAccessLayer/Author.java
@@ -1,17 +1,11 @@
 package com.champsoft.librarymanagementsystem.DataAccessLayer;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.antlr.v4.runtime.misc.NotNull;
-
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +18,20 @@ import java.util.List;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Author {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private String biography;
     private String eMail;
-    //private List<Book> books;
+    
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private List<Book> books = new ArrayList<>();
 
     public Author(String name, String biography, String eMail) {
         this.name = name;
         this.biography = biography;
         this.eMail = eMail;
+        this.books = new ArrayList<>();
     }
 }
