e629ff88da02366d7288497ad60cab1a2eb2d6d1
XavierBradley <xavbrdl@gmail.com>
2025-11-30 14:03:37 -0500

    added a config file to make the backend fuck with the front end

---
+++ LibraryManagementSystem/src/main/java/com/champsoft/librarymanagementsystem/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.champsoft.librarymanagementsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://javalibraryapp.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
